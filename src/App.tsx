d99a802e00c564e4d04438419aac78aad8d24212
Henrique Broering Rohden <hrohden@gmail.com>
2023-08-07 10:55:03 -0300

    Adds some styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 function App() {
   return (
-    <>
-      <h1>Hello! I'm Henrique</h1>
-      <h2>I'm a Fullstack Developer. And this is my portfolio.</h2>
-    </>
+    <main className="flex p-4 h-screen">
+      <div className="flex-col m-auto flex gap-8">
+        <h1 className="font-serif text-8xl text-center">Hello! I'm Henrique</h1>
+        <h2 className="font-serif text-6xl text-center">I'm a Fullstack Developer. And this is my portfolio.</h2>
+      </div>
+    </main>
   )
 }
 
