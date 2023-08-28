44e86f0cd9b71ff75e75a3f42c894ecb7fbbe15e
Henrique Broering Rohden <hrohden@gmail.com>
2023-08-27 21:20:00 -0300

    Fixes LinkedIn URL

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
         <h2 className="font-serif text-4xl text-center">I'm a Fullstack Developer.</h2>
         <h2 className="font-serif text-4xl text-center">(and this is my personal page)</h2>
         <div className="text-center">
-          <a href="https://www.linkedin.com/in/henrique-rohden/?locale=en_US" target="_blank" className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">LinkedIn</a>
+          <a href="https://www.linkedin.com/in/henriquerohden/?locale=en_US" target="_blank" className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">LinkedIn</a>
           <a href="https://github.com/hrohden" target="_blank" className="bg-gray-200 hover:bg-gray-300 font-bold py-2 px-4 rounded ml-2">GitHub</a>
         </div>
       </div>
