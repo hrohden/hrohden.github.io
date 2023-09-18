54168a2a84f5ef0b52d510f86349142da7663d77
Henrique Broering Rohden <hrohden@gmail.com>
2023-09-18 17:45:33 -0300

    First section

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 function App() {
   return (
-    <main className="flex p-4 h-screen">
-      <div className="flex-col m-auto flex gap-8">
-        <h1 className="font-serif text-6xl text-center">Hello! I'm Henrique</h1>
-        <h2 className="font-serif text-4xl text-center">I'm a Fullstack Developer.</h2>
-        <h2 className="font-serif text-4xl text-center">(and this is my personal page)</h2>
+    <main>
+      <section className="bg-gray-900 p-6">
+        <div className="max-w-7xl flex mx-auto justify-evenly">
+          <div className="flex justify-end flex-col gap-6">
+            <h1 className="text-white">Hello! I'm Henrique</h1>
+            <p className="text-white">(it's pronounced /ẽ'ʁiki/)</p>
+            <h2 className="text-white">I'm a Fullstack Developer.</h2>
+          </div>
+          <div>
+            <img src="https://placehold.co/200" alt="" />
+          </div>
+        </div>
+      </section>
+      {/* <div className="flex-col m-auto flex gap-8">\
         <div className="text-center">
           <a href="https://www.linkedin.com/in/henriquerohden/?locale=en_US" target="_blank" className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">LinkedIn</a>
           <a href="https://github.com/hrohden" target="_blank" className="bg-gray-200 hover:bg-gray-300 font-bold py-2 px-4 rounded ml-2">GitHub</a>
         </div>
-      </div>
+      </div> */}
     </main>
   )
 }
