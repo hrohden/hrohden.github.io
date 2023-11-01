960468195f8355f46bce55be1660278dae157a9e
Henrique Broering Rohden <hrohden@gmail.com>
2023-11-01 14:15:33 -0300

    Update portfolio page layout and content

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,41 @@
 function App() {
   return (
     <main className="container mx-auto">
-      <nav className="flex justify-center py-6">
+      <nav className="flex justify-center py-6 border-b-2 border-slate-200">
         <ul className="list-none flex gap-16">
-          <li>
+          <li className="text-center">
             <a href="#" className="font-semibold text-sm">
               About
             </a>
           </li>
-          <li>
+          <li className="text-center">
             <a href="#" className="font-semibold text-sm">
               Work experience
             </a>
           </li>
-          <li>
+          <li className="text-center">
             <a href="#" className="font-semibold text-sm">
               Contact
             </a>
           </li>
-          <li>
+          <li className="text-center">
             <a href="#" className="font-semibold text-sm">
               Resumé
             </a>
           </li>
         </ul>
       </nav>
-      <section className="py-12">
-        <p>Hello! I'm Henrique</p>
-        <p>Welcome to my portfolio! 🙂</p>
+      <section className="py-16 flex flex-col items-center">
+        <p>
+          <h1 className="text-6xl font-semibold tracking-tighter">
+            Hello! I'm Henrique
+          </h1>
+        </p>
+        <p>
+          <h2 className="text-4xl font-thin tracking-tighter">
+            Welcome to my portfolio! 🙂
+          </h2>
+        </p>
       </section>
       <section className="py-12 bg-slate-400">
         <h2 className="text-4xl font-semibold tracking-tight">About</h2>
