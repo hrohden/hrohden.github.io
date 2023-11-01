69f294d0d9c9b674720fa9e399c260d3d4d5ac55
Henrique Broering Rohden <hrohden@gmail.com>
2023-11-01 14:58:39 -0300

    Added work experience items to src/workExperienceItems.ts

--- src/workExperienceItems.ts
+++ src/workExperienceItems.ts
@@ -21,4 +21,41 @@ export const workExperienceItems: WorkExperience[] = [
     ],
     technologies: ["Java", "Spring Boot", "Spring Framework", "React", "SQL"],
   },
+  {
+    company: "Cooperforte Ltda.",
+    title: "Senior Java developer",
+    period: "July 2021 - November 2021",
+    description: [
+      "Developed and maintained web applications using React, Java, Spring, and SQL Server",
+    ],
+    technologies: ["Java", "Spring Framework", "React", "SQL", "AWS"],
+  },
+  {
+    company: "Stefanini",
+    title: "Senior developer",
+    period: "August 2020 - July 2021",
+    description: [
+      "Developed and maintained web applications for banking corporations, using front-end technologies (Angular, Grunt, Jasmine, Karma).",
+    ],
+    technologies: ["Java", "Angular", "SQL"],
+  },
+  {
+    company: "Basis Tecnologia",
+    title: "Senior Java developer",
+    period: "December 2019 - August 2020",
+    description: [
+      "Developed and maintained web applications for federal environmental agencies, using Java-based stack (Spring, JPA) as well as CI/CD tools (Jenkins), OpenShift, and Kubernetes",
+    ],
+    technologies: ["Java", "Spring", "Jenkins", "Docker", "Angular", "SQL"],
+  },
+  {
+    company: "Banco do Brasil",
+    title: "Software Developer",
+    period: "February 2011 - September 2019",
+    description: [
+      "Successfully developed from scratch a high-quality official products website for employees' usage, making use of Java, JSP (Servlets), and frameworks like Spring MVC and JPA/Hibernate, resulting in +2,500 daily hits, one of the company's intranet most popular web applications",
+      "Responsible for managing databases and developing internal performance reports of financial products, using HTML, CSS, JavaScript, and jQuery, as well as MySQL, SQL Server, and Postgres",
+    ],
+    technologies: ["Java", "Spring", "React", "Angular", "SQL"],
+  },
 ];
