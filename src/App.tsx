25be179e9668cec4a5072958c4d87989f2466e82
Henrique Broering Rohden <hrohden@gmail.com>
2023-11-01 14:05:49 -0300

    Add styling and update class names

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
 function App() {
   return (
-    <main>
-      <nav>
-        <ul>
-          <li>About</li>
-          <li>Work experience</li>
-          <li>Contact</li>
-          <li>Resumé</li>
+    <main className="container mx-auto">
+      <nav className="flex justify-center py-6">
+        <ul className="list-none flex gap-16">
+          <li>
+            <a href="#" className="font-semibold text-sm">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#" className="font-semibold text-sm">
+              Work experience
+            </a>
+          </li>
+          <li>
+            <a href="#" className="font-semibold text-sm">
+              Contact
+            </a>
+          </li>
+          <li>
+            <a href="#" className="font-semibold text-sm">
+              Resumé
+            </a>
+          </li>
         </ul>
       </nav>
-      <section>
+      <section className="py-12">
         <p>Hello! I'm Henrique</p>
         <p>Welcome to my portfolio! 🙂</p>
       </section>
-      <section>
-        <h2>About</h2>
+      <section className="py-12 bg-slate-400">
+        <h2 className="text-4xl font-semibold tracking-tight">About</h2>
       </section>
-      <section>
-        <h2>Work experience</h2>
+      <section className="py-12">
+        <h2 className="text-4xl font-semibold tracking-tight">
+          Work experience
+        </h2>
       </section>
-      <section>
-        <h2>Contact</h2>
+      <section className="py-12 bg-slate-400">
+        <h2 className="text-4xl font-semibold tracking-tight">Contact</h2>
       </section>
     </main>
   );
