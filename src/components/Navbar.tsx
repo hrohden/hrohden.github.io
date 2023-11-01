9106a8670755ef9354731b2e950559bb85b4a1b7
Henrique Broering Rohden <hrohden@gmail.com>
2023-11-01 16:08:20 -0300

    Add icons and update links in Navbar component.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,36 @@
+import {
+  BsCloudDownloadFill,
+  BsFillBriefcaseFill,
+  BsInfoCircleFill,
+} from "react-icons/bs";
+import { IoMail } from "react-icons/io5";
+
 const Navbar = () => {
   return (
     <nav className="flex justify-center py-6 border-b-2 border-black">
       <ul className="list-none flex gap-16">
         <li className="text-center">
           <a href="#" className="font-semibold text-sm">
+            <BsInfoCircleFill />
             About
           </a>
         </li>
         <li className="text-center">
           <a href="#" className="font-semibold text-sm">
+            <BsFillBriefcaseFill />
             Work experience
           </a>
         </li>
         <li className="text-center">
           <a href="#" className="font-semibold text-sm">
+            <IoMail />
             Contact
           </a>
         </li>
         <li className="text-center">
           <a href="#" className="font-semibold text-sm">
-            Resumé
+            <BsCloudDownloadFill />
+            Resumé PDF
           </a>
         </li>
       </ul>
