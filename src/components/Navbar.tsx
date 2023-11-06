5af12312683929d0401d06cdf00cca5d72f69104
Henrique Broering Rohden <hrohden@gmail.com>
2023-11-06 14:24:11 -0300

    Refactor Navbar component layout and styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,28 +7,28 @@ import { IoMail } from "react-icons/io5";
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-center py-6 border-b-2 border-black">
-      <ul className="list-none flex gap-16">
+    <nav className="py-2 border-b-2 border-black">
+      <ul className="list-none flex gap-2 justify-between md:justify-evenly text-sm font-semibold">
         <li className="text-center">
-          <a href="#" className="font-semibold text-sm">
+          <a href="#" className="flex gap-1 items-center">
             <BsInfoCircleFill />
             About
           </a>
         </li>
         <li className="text-center">
-          <a href="#" className="font-semibold text-sm">
+          <a href="#" className="flex gap-1 items-center">
             <BsFillBriefcaseFill />
-            Work experience
+            Experience
           </a>
         </li>
         <li className="text-center">
-          <a href="#" className="font-semibold text-sm">
+          <a href="#" className="flex gap-1 items-center">
             <IoMail />
             Contact
           </a>
         </li>
         <li className="text-center">
-          <a href="#" className="font-semibold text-sm">
+          <a href="#" className="flex gap-1 items-center">
             <BsCloudDownloadFill />
             Resumé PDF
           </a>
