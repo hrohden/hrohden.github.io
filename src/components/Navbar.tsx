add6d347d5bc1f4aa5b6e11b43e3abe096f5a57a
Henrique Broering Rohden <hrohden@gmail.com>
2023-11-06 16:31:02 -0300

    Remove icons from Navbar links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,35 +1,25 @@
-import {
-  BsCloudDownloadFill,
-  BsFillBriefcaseFill,
-  BsInfoCircleFill,
-} from "react-icons/bs";
-import { IoMail } from "react-icons/io5";
 
 const Navbar = () => {
   return (
     <nav className="border-b-2 border-black py-2">
-      <ul className="flex list-none justify-between gap-2 text-sm font-semibold tracking-tight md:justify-evenly">
+      <ul className="flex list-none gap-2 text-sm font-semibold tracking-tight md:justify-evenly">
         <li className="text-center">
           <a href="#" className="flex items-center gap-1">
-            <BsInfoCircleFill />
             About
           </a>
         </li>
         <li className="text-center">
           <a href="#" className="flex items-center gap-1">
-            <BsFillBriefcaseFill />
             Experience
           </a>
         </li>
         <li className="text-center">
           <a href="#" className="flex items-center gap-1">
-            <IoMail />
             Contact
           </a>
         </li>
         <li className="text-center">
           <a href="#" className="flex items-center gap-1">
-            <BsCloudDownloadFill />
             Resumé PDF
           </a>
         </li>
