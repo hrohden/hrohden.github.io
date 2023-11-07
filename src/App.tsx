1ba51f1a5c2c08ca2e5a1e44b261da62749e1168
Henrique Broering Rohden <hrohden@gmail.com>
2023-11-07 11:35:54 -0300

    Update personal page with new name and links

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,34 @@ function App() {
   return (
     <main className="flex p-4 h-screen">
       <div className="flex-col m-auto flex gap-8">
-        <h1 className="font-serif text-6xl text-center">Hello! I'm Henrique</h1>
-        <h2 className="font-serif text-4xl text-center">I'm a Fullstack Developer.</h2>
-        <h2 className="font-serif text-4xl text-center">(and this is my personal page)</h2>
+        <h1 className="font-serif text-6xl text-center">
+          Hello! I'm Henrique Rohden!
+        </h1>
+        <h2 className="font-serif text-4xl text-center">
+          I'm a Fullstack Developer.
+        </h2>
+        <h2 className="font-serif text-4xl text-center">
+          (and this is my personal page)
+        </h2>
         <div className="text-center">
-          <a href="https://www.linkedin.com/in/henriquerohden/?locale=en_US" target="_blank" className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">LinkedIn</a>
-          <a href="https://github.com/hrohden" target="_blank" className="bg-gray-200 hover:bg-gray-300 font-bold py-2 px-4 rounded ml-2">GitHub</a>
+          <a
+            href="https://www.linkedin.com/in/henriquerohden/?locale=en_US"
+            target="_blank"
+            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+          >
+            LinkedIn
+          </a>
+          <a
+            href="https://github.com/hrohden"
+            target="_blank"
+            className="bg-gray-200 hover:bg-gray-300 font-bold py-2 px-4 rounded ml-2"
+          >
+            GitHub
+          </a>
         </div>
       </div>
     </main>
-  )
+  );
 }
 
 export default App
