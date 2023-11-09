4c66eaa98392873d1062e10b714ea2b05bd46c71
Henrique Broering Rohden <hrohden@gmail.com>
2023-11-09 13:38:26 -0300

    Add download link to resume in HeroActionButton component

--- src/components/HeroActionButton.tsx
+++ src/components/HeroActionButton.tsx
@@ -4,7 +4,9 @@ const HeroActionButton = () => {
   return (
     <p className="text-center">
       <a
-        href=""
+        href="https://henriquerohden.s3.amazonaws.com/Resume.pdf"
+        download="Resume - Henrique Rohden.pdf"
+        target="_blank"
         className="inline-flex w-3/4 items-center justify-center gap-2 rounded-full bg-gradient-to-b 
           from-sky-500 to-blue-600 px-4 py-3 text-lg font-semibold text-white
            shadow-md hover:bg-gradient-to-b hover:from-sky-400 hover:to-blue-500 focus:ring md:w-1/4"
