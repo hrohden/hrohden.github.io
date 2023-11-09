ca08496965c453a6c1f9013481e784dd54109acc
Henrique Broering Rohden <hrohden@gmail.com>
2023-11-09 10:54:34 -0300

    Refactor SocialMediaButton component props

--- src/components/SocialMediaButton.tsx
+++ src/components/SocialMediaButton.tsx
@@ -3,12 +3,12 @@ type Props = {
   icon: React.ReactNode;
   url: string;
 };
-const SocialMediaButton = (props: Props) => {
+const SocialMediaButton = ({ url, icon, text }: Props) => {
   return (
-    <a href={props.url} target="_blank">
+    <a href={url} target="_blank">
       <span className="inline-flex flex-row items-center gap-2 rounded-md border border-slate-200 bg-white p-3 shadow-sm">
-        <span className="text-slate-400">{props.icon}</span>
-        {props.text && <span className="text-xs md:text-sm">{props.text}</span>}
+        <span className="text-slate-400">{icon}</span>
+        {text && <span className="text-xs md:text-sm">{text}</span>}
       </span>
     </a>
   );
