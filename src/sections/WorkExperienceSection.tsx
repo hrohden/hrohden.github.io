ee01eeb3b85478ed984d5b76ab83f701a0f07b5d
Henrique Broering Rohden <hrohden@gmail.com>
2023-11-09 15:19:33 -0300

    Update gap between work experience items in WorkExperienceSection

--- src/sections/WorkExperienceSection.tsx
+++ src/sections/WorkExperienceSection.tsx
@@ -5,7 +5,7 @@ import { workExperienceItems } from "../data/workExperienceItems";
 const WorkExperienceSection = () => {
   return (
     <Section title="Work experience">
-      <div className="flex flex-col gap-6">
+      <div className="flex flex-col gap-9">
         {workExperienceItems.map((work) => (
           <WorkExperienceComponent workExperience={work} key={work.company} />
         ))}
