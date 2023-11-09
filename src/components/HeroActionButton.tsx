6d6b8fd7ff0d6eaac4e72083debd3fbaa4df838f
Henrique Broering Rohden <hrohden@gmail.com>
2023-11-09 15:18:01 -0300

    Update HeroActionButton and Navbar styles

--- src/components/HeroActionButton.tsx
+++ src/components/HeroActionButton.tsx
@@ -7,9 +7,9 @@ const HeroActionButton = () => {
         href="https://henriquerohden.s3.amazonaws.com/Resume.pdf"
         download="Resume - Henrique Rohden.pdf"
         target="_blank"
-        className="inline-flex w-3/4 items-center justify-center gap-2 rounded-full bg-gradient-to-b 
-          from-sky-500 to-blue-600 px-4 py-3 text-lg font-semibold text-white
-           shadow-md hover:bg-gradient-to-b hover:from-sky-400 hover:to-blue-500 focus:ring md:w-1/4"
+        className="inline-flex w-3/4 items-center justify-center gap-2 rounded-full border-b border-t border-b-sky-700 border-t-sky-300 bg-gradient-to-b from-sky-500 to-blue-600 px-4 py-3 
+          text-lg font-semibold text-white shadow-md hover:border-b hover:border-t hover:border-b-sky-600
+           hover:border-t-sky-200 hover:bg-gradient-to-b hover:from-sky-400 hover:to-blue-500 focus:ring md:w-1/4"
       >
         <BsCloudDownload className=" text-sky-200" />
         Resumé PDF
