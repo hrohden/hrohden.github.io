3fe4e211de20174395773c47668c3663bddea03e
Henrique Broering Rohden <hrohden@gmail.com>
2023-11-09 10:53:38 -0300

    Refactor SocialMediaButton component to make text prop optional

--- src/components/SocialMediaButton.tsx
+++ src/components/SocialMediaButton.tsx
@@ -1,14 +1,14 @@
 type Props = {
-  text: string;
+  text?: string;
   icon: React.ReactNode;
   url: string;
 };
 const SocialMediaButton = (props: Props) => {
   return (
     <a href={props.url} target="_blank">
-      <span className="inline-flex flex-row items-center gap-2 rounded-md border border-slate-200 bg-slate-100 px-3 py-2 shadow-sm">
+      <span className="inline-flex flex-row items-center gap-2 rounded-md border border-slate-200 bg-white p-3 shadow-sm">
         <span className="text-slate-400">{props.icon}</span>
-        <span className="text-xs md:text-sm">{props.text}</span>
+        {props.text && <span className="text-xs md:text-sm">{props.text}</span>}
       </span>
     </a>
   );
