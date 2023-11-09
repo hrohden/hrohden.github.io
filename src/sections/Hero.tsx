9db1c6d94af3e482236c739e1ae240da2f8f1037
Henrique Broering Rohden <hrohden@gmail.com>
2023-11-09 10:53:26 -0300

    Refactor About and Hero sections

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,25 +1,35 @@
-import { FaGithub, FaLinkedin } from "react-icons/fa";
-import { RiTwitterXFill } from "react-icons/ri";
-import me from "../assets/me.jpg";
-import SocialMediaButton from "../components/SocialMediaButton";
+import { BsCloudDownload } from "react-icons/bs";
 
 const Hero = () => {
   return (
-    <section className="grid grid-cols-4 gap-2 py-8">
-      <div className="col-span-3 flex flex-row items-center">
-        <div className="flex w-3/4 flex-col gap-1">
-          <p className="text-sm font-semibold uppercase tracking-tight text-sky-500">
-            Full stack developer
-          </p>
-          <h1 className="text-2xl font-extrabold tracking-tighter text-slate-700 sm:text-5xl sm:leading-[3.5rem]">
-            Hello! I'm Henrique
-          </h1>
-          <h2 className="mb-2 text-lg font-thin tracking-tighter text-slate-700">
-            Welcome to my portfolio! 🚀
-          </h2>
-        </div>
-      </div>
-      <div>
+    <section className="flex flex-col gap-3 py-10">
+      {/* <div className="col-span-3 flex flex-row items-center"> */}
+      {/* <div className="flex w-3/4 flex-col gap-1"> */}
+      <p className="text-center text-xl font-semibold text-slate-500">
+        Full stack developer
+      </p>
+      <h1 className="mb-3 text-center text-6xl font-bold tracking-tight text-slate-800">
+        Hello! I'm Henrique
+      </h1>
+      <p className="mb-6 text-center text-xl text-slate-800">
+        With experience in frontend and backend development, I have consistently
+        leveraged my technical skills to create efficient, secure, and
+        user-friendly applications.
+      </p>
+      <p className="text-center">
+        <a
+          href=""
+          className="inline-flex w-3/4 items-center justify-center gap-2 rounded-full bg-gradient-to-b 
+          from-sky-500 to-blue-600 px-4 py-3 text-lg font-semibold text-white
+           shadow-md hover:bg-gradient-to-b hover:from-sky-400 hover:to-blue-500 focus:ring md:w-1/4"
+        >
+          <BsCloudDownload className=" text-sky-200" />
+          Resumé PDF
+        </a>
+      </p>
+      {/* </div> */}
+      {/* </div> */}
+      {/* <div>
         <img src={me} className="w-full rounded-lg" />
       </div>
       <ul className="flex list-none gap-2">
@@ -44,7 +54,7 @@ const Hero = () => {
             text="Twitter"
           />
         </li>
-      </ul>
+      </ul> */}
     </section>
   );
 };
